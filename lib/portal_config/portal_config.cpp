7526e69df993df7ced3cc67495675c604cf26b8e
Skyhwk <dedi@intilab.com>
2026-07-25 11:18:20 +0700

    todo berikutnya

--- lib/portal_config/portal_config.cpp
+++ lib/portal_config/portal_config.cpp
@@ -328,6 +328,7 @@ bool PortalConfig::isApMode() const { return g_apMode; }
 
 bool PortalConfig::beginApOnHold(uint8_t buttonPin, uint32_t holdMs)
 {
+    // TODO: Masih bug di sini, jika tombol ditekan, maka AP akan terbuka, tetapi jika tombol dilepas, maka AP akan tertutup
     pinMode(buttonPin, INPUT_PULLUP);
 
     if (digitalRead(buttonPin) == LOW)
